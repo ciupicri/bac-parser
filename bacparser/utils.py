d884b4d8bec1b8b2fabeebffbeb18a353b79db24
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-07-15 14:13:10 +0300

    Add utils module with grouper function

---
+++ bacparser/utils.py
@@ -0,0 +1,6 @@
+import itertools
+
+def grouper(n, iterable, fillvalue=None):
+    "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return itertools.izip_longest(fillvalue=fillvalue, *args)
