ddd224ab3348ae71bf3f37db28ad0c3e6f2a6b59
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2012-07-14 17:50:49 +0300

    bacparser.models: use Elev 2010 data model for 2012
    The data model for the results hasn't changed fortunately.

--- bacparser/models/__init__.py
+++ bacparser/models/__init__.py
@@ -2,14 +2,13 @@ from .. import BacParserUnsupportedYear
 from . import elev2006
 from . import elev2010
 
-SUPPORTED_YEARS = range(2006, 2011+1)
+SUPPORTED_YEARS = range(2006, 2012+1)
 
 def get_model(year):
     if year < 2006:
         raise BacParserValueError('Unsupported year')
     if year <= 2009:
         return elev2006.Elev
-    if year <= 2011:
+    if year <= 2012:
         return elev2010.Elev
-    # year > 2011
     raise BacParserUnsupportedYear('Unsupported year')
