9895d40296139cf409f82c57a2a922f77def1dc7
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2012-07-14 19:02:59 +0300

    cosmetic changes in bacparser/parsers/__init__.py
    Use the same style as in bacparser/models/__init__.py

--- bacparser/parsers/__init__.py
+++ bacparser/parsers/__init__.py
@@ -7,15 +7,14 @@ from .parser2010 import Parser2010
 SUPPORTED_YEARS = range(2006, 2011+1)
 
 def get_parser(year):
-    if year > 2011:
+    if year < 2006:
         raise BacParserUnsupportedYear('Unsupported year')
-    if year >= 2010:
-        return Parser2010
-    if year >= 2009:
-        return Parser2009
-    if year >= 2007:
-        return Parser2007
-    if year >= 2006:
+    if year <= 2006:
         return Parser2006
-    # year < 2006
+    if year <= 2008:
+        return Parser2007
+    if year <= 2009:
+        return Parser2009
+    if year <= 2011:
+        return Parser2010
     raise BacParserUnsupportedYear('Unsupported year')
