690a62126f5312806c5349ef4ce4e8f0417ec00f
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2012-07-14 21:13:52 +0300

    bacparser.maintable: add support for year 2012
    We're finally parsing plain HTML pages, but the table does not have the
    id HTML attribute anymore. Instead we have to rely on the class attribute.

--- bacparser/maintable.py
+++ bacparser/maintable.py
@@ -1,35 +1,45 @@
+__docformat__ = "restructuredtext en"
+
 import lxml.etree
 import lxml.html
 import sys
 
 from .ged import get_inner_html
 
-def get_main_table(html):
-    """ -> the mainTable table"""
-    html = html[html.index('<HTML>'):]
+def get_main_table(html, year):
+    """Return the mainTable table, the one containing the results"""
     html = unicode(html, 'utf-8')
     doc = lxml.html.fromstring(html)
-    main_table = doc.xpath(r'//table[@id="mainTable"]')[0]
-    return main_table
+    if year <= 2011:
+        return doc.xpath(r'//table[@id="mainTable"]')[0]
+    else:
+        return doc.xpath(r'//table[@class="mainTable"]')[0]
 
 def get_main_table_from_file(f, year):
-    """ -> the mainTable table from a file"""
-    if year <= 2009:
-        html = f.read()
-    else:
-        for line in f:
-            html = get_inner_html(line)
-            if html:
-                break
-        else:
-            return None
-    return get_main_table(html)
+    """Return the ``mainTable`` table from file ``f`` for year ``year`` while
+        taking care of all the obfuscation and garbage.
+    """
 
-def main(f):
+    if year <= 2011:
+        if year <= 2009:
+            html = f.read()
+        else: # Siveco's HTML inside HTML
+            for line in f:
+                html = get_inner_html(line)
+                if html:
+                    break
+            else:
+                return None
+        html = html[html.index('<HTML>'):] # remove leading garbage
+    else: # finally a plain HTML
+        html = f.read()
+    return get_main_table(html, year)
+
+def main(f, year):
     with f:
         html = f.read()
         main_table = get_main_table(html)
         sys.stdout.write(lxml.etree.tostring(main_table, pretty_print=True))
 
 if __name__ == '__main__':
-    main(sys.stdin)
+    main(sys.stdin, int(sys.argv[1]))
