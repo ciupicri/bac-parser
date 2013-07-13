0ec4a8508613747a1f6a5fb17533bf0ed5425294
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2013-07-13 18:40:08 +0300

    Add support for the 2013 exam

--- bacparser/models/__init__.py
+++ bacparser/models/__init__.py
@@ -2,13 +2,13 @@ from .. import BacParserUnsupportedYear
 from . import elev2006
 from . import elev2010
 
-SUPPORTED_YEARS = range(2006, 2012+1)
+SUPPORTED_YEARS = range(2006, 2013+1)
 
 def get_model(year):
     if year < 2006:
         raise BacParserValueError('Unsupported year')
     if year <= 2009:
         return elev2006.Elev
-    if year <= 2012:
+    if year <= 2013:
         return elev2010.Elev
     raise BacParserUnsupportedYear('Unsupported year')
